ec1df196016963bfdf0f15a90d03dfb48f6aadcd
Jordan Barrios <jbarrios2017@fau.edu>
2020-10-31 23:34:59 -0400

    Added sample overlap code to map, non-function

--- HW8-5/app/src/main/java/com/example/hw8_5/DisplayWeatherMap.java
+++ HW8-5/app/src/main/java/com/example/hw8_5/DisplayWeatherMap.java
@@ -15,12 +15,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.maps.model.TileProvider;
+import com.google.android.gms.maps.model.UrlTileProvider;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DisplayWeatherMap extends AppCompatActivity implements OnMapReadyCallback {
 
+    private String openWeatherKey = "130ecc705ac2dd85de148abbeff2d7ff";
     private GoogleMap mMap;
-
     private Context context;
 
     @Override
@@ -69,7 +76,31 @@ public class DisplayWeatherMap extends AppCompatActivity implements OnMapReadyCa
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        TileProvider tileProvider = new UrlTileProvider(256, 256) {
+            @Override
+            public URL getTileUrl(int x, int y, int zoom) {            /* Define the URL pattern for the tile images */
+            String s = String.format("https://tile.openweathermap.org/map/temp_new/%d/%d/%d.png?appid="+openWeatherKey+"", zoom, x, y);
+                if (!checkTileExists(x, y, zoom)) {
+                    return null;
+                }            try {
+                    return new URL(s);
+                } catch (MalformedURLException e) {
+                    throw new AssertionError(e);
+                }
+            }
+            /*
+            * Check that the tile server supports the requested x, y and zoom.
+             * Complete this stub according to the tile range you support.
+            * If you support a limited range of tiles at different zoom levels, then you
+            * need to define the supported x, y range at each zoom level.
+            */
+            private boolean checkTileExists(int x, int y, int zoom) {
+                int minZoom = 12;
+                int maxZoom = 16;
+                return (zoom >= minZoom && zoom <= maxZoom);
+            }
+        };
+        //mMap = googleMap;
 
         WeatherData data = MainActivity.getWeatherInstance();
 
@@ -79,7 +110,10 @@ public class DisplayWeatherMap extends AppCompatActivity implements OnMapReadyCa
 
         // Add a marker and move the camera
         LatLng location = new LatLng(latitude, longitude);
-        mMap.addMarker(new MarkerOptions().position(location).title(data.getName() + ", " + data.getCountry()));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        googleMap.addMarker(new MarkerOptions().position(location).title(data.getName() + ", " + data.getCountry()));
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 8.0f));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(5));
+        TileOverlay tileOverlay = googleMap.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
     }
 }
