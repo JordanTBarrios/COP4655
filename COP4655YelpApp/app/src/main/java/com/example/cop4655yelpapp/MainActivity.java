ea47baa6b6d3d79bbcc456b7106bc7b340d350cb
Jordan Barrios <jbarrios2017@fau.edu>
2020-12-01 21:50:27 -0500

    Initial Commit

---
+++ COP4655YelpApp/app/src/main/java/com/example/cop4655yelpapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.cop4655yelpapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void onSignInClick(){
+
+    }
+
+    private void onSignUpClick(){
+
+    }
+}
