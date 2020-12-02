ec27d1f9b6b0e46295fe2dcdc321bd43f549e5f9
Jordan Barrios <jbarrios2017@fau.edu>
2020-12-02 14:55:28 -0500

    Added basic layouts for the various activities

---
+++ COP4655YelpApp/app/src/main/java/com/example/cop4655yelpapp/MapActivity.java
@@ -0,0 +1,14 @@
+package com.example.cop4655yelpapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
