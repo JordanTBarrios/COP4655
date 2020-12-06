83562cf340915d0fac3593de8a9b611fd9e8bca5
Jordan Barrios <jbarrios2017@fau.edu>
2020-12-06 11:45:11 -0500

    Added Sign In functionality

--- COP4655YelpApp/app/src/main/java/com/example/cop4655yelpapp/MainActivity.java
+++ COP4655YelpApp/app/src/main/java/com/example/cop4655yelpapp/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.cop4655yelpapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private Context context;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN = 1;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +38,108 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         context = getApplicationContext();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //google signin
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("1037212829282-4fra8k5qfi2smk16cp2idoh86hef48mh.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+        //R.string.default_web_client_id
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        findViewById(R.id.SignInButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                onSignInClick(view);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //updateUI(account);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+            startActivity(intent);
+        }
+        //updateUI(currentUser);
     }
 
     public void onSignInClick(View view){
-        Intent SearchIntent = new Intent (context, MapActivity.class);
-        startActivity(SearchIntent);
+        //Intent SearchIntent = new Intent (context, MapActivity.class);
+        //startActivity(SearchIntent);
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     public void onSignUpClick(View view){
 
     }
+
+    public void updateUI(FirebaseUser currentUse){
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                // ...
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            // mBinding.mainLayout
+                            Toast.makeText(MainActivity.this, "Authentication failure", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    /*
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    */
 }
--- COP4655YelpApp/app/src/main/java/com/example/cop4655yelpapp/MapActivity.java
+++ COP4655YelpApp/app/src/main/java/com/example/cop4655yelpapp/MapActivity.java
@@ -1,8 +1,13 @@
 package com.example.cop4655yelpapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,6 +17,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -23,6 +30,42 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //listener for the bottom navigation view
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch(item.getItemId()){
+                            case R.id.home:
+                                /*
+                                Toast.makeText(DisplayWeatherMap.this, "Results", Toast.LENGTH_SHORT).show();
+                                Intent resultsIntent = new Intent (context, DisplayWeatherResults.class);
+                                startActivity(resultsIntent); //is last
+                                break;
+
+                                 */
+                            case R.id.map:
+                                /*
+                                Toast.makeText(DisplayWeatherMap.this, "Map", Toast.LENGTH_SHORT).show();
+                                break;
+
+                                 */
+                            case R.id.more_icon:
+                                //Intent historyIntent = new Intent (getApplicationContext(), DisplayWeatherHistory.class);
+                                //startActivity(historyIntent); //is last
+                                //MenuItem logout = findViewById(R.id.more_icon);
+                                FirebaseAuth.getInstance().signOut();
+                                Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+                                startActivity(intent); //is last
+
+                                break;
+                        }
+                        return true;
+                    }
+                }
+        );
     }
 
     @Override
